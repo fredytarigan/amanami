f9cb94de8b36e13e8bbb86578aaba6af01ebf1d5
Fredy Samuel B Tarigan <fredy.tarigan@tiptip.tv>
2024-03-21 17:37:59 +0700

    start working with regex

--- crates/amanami/src/grafana.rs
+++ crates/amanami/src/grafana.rs
@@ -1,4 +1,6 @@
 use crate::config::GrafanaConfig;
+use regex::Regex;
+use serde_json::Value;
 
 #[derive(Debug, Clone)]
 pub struct Grafana {
@@ -29,21 +31,40 @@ impl Grafana {
 
     pub fn get_grafana_update(&self) {
         for instance in self.instances.clone() {
-            get_grafana_version(instance);
+            let version = get_grafana_version(instance);
+
+            get_grafana_latest_version(version);
         }
     }
 }
 
 #[::tokio::main]
-async fn get_grafana_version(instance: GrafanaInstance) {
+async fn get_grafana_version(instance: GrafanaInstance) -> String {
     const GRAFANA_API_PATH: &str = "/api/health";
 
     let response = reqwest::get(format!("{}{}", instance.url, GRAFANA_API_PATH))
         .await
         .unwrap();
 
-    println!("Status: {}", response.status());
+    let body = response.text().await.unwrap();
+    let json_data = serde_json::from_str::<Value>(&body).unwrap();
 
-    let body = response.text().await;
-    println!("Body: {:?}", body);
+    json_data.get("version").unwrap().to_string()
+}
+
+#[::tokio::main]
+async fn get_grafana_latest_version(curr_version: String) {
+    const GRAFANA_CHANGELOG_URL: &str =
+        "https://raw.githubusercontent.com/grafana/grafana/main/CHANGELOG.md";
+
+    let response = reqwest::get(format!("{}", GRAFANA_CHANGELOG_URL))
+        .await
+        .unwrap();
+
+    let body = response.text().await.unwrap();
+
+    let re = Regex::new(r"^(0|[1-9][0-9]*)\.(0|[1-9][0-9]*)\.(0|[1-9][0-9]*)(-[0-9A-Za-z\.-]+)?(\+[0-9A-Za-z\.-]+)?$|^Unreleased$").unwrap();
+    let caps = re.captures(&body).unwrap();
+
+    println!("{:?}", caps);
 }
